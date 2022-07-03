068746d6798c7faa604f8f92cc96fa33cff930db
Joel <joel@joel.tokyo>
2022-07-03 18:41:17 +1000

    feat: add inkscape version check

--- mindustry_svg_renderer/__main__.py
+++ mindustry_svg_renderer/__main__.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from logging import getLogger
 from pathlib import Path
+from subprocess import check_output
 
 from rich.progress import track
 
@@ -17,6 +18,14 @@ parser.add_argument("-l", "--log-to", default=Path("inkscape.log"), type=Path)
 def main() -> None:
     args = parser.parse_args()
     log = getLogger(__name__)
+
+    expected_version = "Inkscape 1.2"
+    version = check_output(["inkscape", "--version"]).decode().strip()
+    if version.startswith("Inkscape " + expected_version):
+        log.warn(
+            "You may be running an unsupported version of inkscape. "
+            + f"Expected {expected_version}, found {version}"
+        )
 
     log.info(f"Redirecting Inkscape's STDOUT and STDERR to {args.log_to}")
     with open(args.log_to, "wb") as log_to:
