2b03b4b561e2d5014f27a404b22196195a87bf3f
Joel <joel@joel.tokyo>
2022-07-03 21:30:10 +1000

    fix: add delay after pressing export button
    failed with core/assets-raw/sprites_out/ui/block-core-acropolis-ui.png

--- mindustry_svg_renderer/__init__.py
+++ mindustry_svg_renderer/__init__.py
@@ -121,6 +121,7 @@ def render(path: Path, *args: Any, **kwargs: Any) -> None:
         # Export
         keyboard.tap(Key.tab)
         keyboard.tap(Key.enter)
+        sleep(1)
 
         # Quit
         tap_multiple(keyboard, [Key.ctrl, "q"])
